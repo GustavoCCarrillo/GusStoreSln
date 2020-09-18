701a901fa6a5a2ffe283c83c3bfb277ba6d0702a
Gustavo Carrillo <g2c100@hotmail.com>
2020-09-17 20:18:13 -0700

    Added more Test cases to the OutdoorProduct.Tests project and implemented a shoping cart feature to the OutdoorProducts web appliction.

---
+++ OutdoorProducts/Pages/Cart.cshtml.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using OutdoorProducts.Infrastructure;
+using OutdoorProducts.Models;
+
+namespace OutdoorProducts.Pages
+{
+    public class CartModel : PageModel
+    {
+        private IStoreRepository repository;
+        public CartModel(IStoreRepository repo)
+        {
+            repository = repo;
+        }
+
+        public Cart Cart { get; set; }
+        public string ReturnUrl { get; set; }
+        public void OnGet(string returnUrl)
+        {
+            ReturnUrl = returnUrl ?? "/";
+            Cart = HttpContext.Session.GetJson<Cart>("cart") ?? new Cart();
+        }
+
+        public IActionResult OnPost(long productId, string returnUrl)
+        {
+            Product product = repository.Products
+                .FirstOrDefault(p => p.ProductID == productId);
+            //ReturnUrl = returnUrl ?? "/";
+            Cart = HttpContext.Session.GetJson<Cart>("cart") ?? new Cart();
+            Cart.AddItem(product, 1);
+            HttpContext.Session.SetJson("cart", Cart);
+            return RedirectToPage(new { returnUrl = returnUrl });
+        }
+    }
+}
